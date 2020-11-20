c0c97022df9eafaa3c04d9dc477f03fca6331b47
Trần Quang Đức <47981725+ductq1999@users.noreply.github.com>
2020-11-20 22:50:31 +0700

    update db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,40 @@ use qlhvt;
  CREATE TABLE Driver (
 `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 `name` VARCHAR(30) NOT NULL,
-`licenseNumber` VARCHAR(30) NOT NULL,
-`licenseType` VARCHAR(30) NOT NULL,
+`license_number` VARCHAR(30) NOT NULL,
+`license_type` VARCHAR(30) NOT NULL,
 `address` VARCHAR(30) NOT NULL,
-`dateOfBirth` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`date_of_birth` timestamp,
 `seniority` VARCHAR(30) NOT NULL
 );
+-- CREATE TABLE Coach (
+-- `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+-- `licensePlate` VARCHAR(30) NOT NULL,
+-- `color` VARCHAR(30) NOT NULL,
+-- `manufacturer` VARCHAR(30) NOT NULL,
+-- `car_type` VARCHAR(30) NOT NULL,
+-- `chair` VARCHAR(30) NOT NULL,
+-- `year` VARCHAR(30) NOT NULL,
+-- `last_maintenance` TIMESTAMP
+-- );
+-- CREATE TABLE  Buses (
+-- `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+-- `first` VARCHAR(30) NOT NULL,
+-- `last` VARCHAR(30) NOT NULL,
+-- `length` VARCHAR(30) NOT NULL,
+-- `complexity` INT(6) NOT NULL -- 1 2 3
+-- );
+
+-- CREATE TABLE  Trip (
+-- `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+-- `code` VARCHAR(30) NOT NULL,
+-- `buses_id` INT(6) UNSIGNED,
+-- `driver_primary` INT(6) UNSIGNED,
+-- `driver_foreign` INT(6) UNSIGNED,
+-- `guest_number` VARCHAR(30) NOT NULL,
+-- `fare` VARCHAR(30) NOT NULL,
+-- FOREIGN KEY (buses_id) REFERENCES Buses(id),
+-- FOREIGN KEY (driver_primary) REFERENCES Driver(id),
+-- FOREIGN KEY (driver_foreign) REFERENCES Driver(id)
+-- );
+-- drop database qlhvt;
